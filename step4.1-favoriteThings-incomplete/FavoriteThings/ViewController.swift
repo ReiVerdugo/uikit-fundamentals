847f53f28e5ae103fa7e9d845ef3b42ccc21e972
Reinaldo Verdugo <reinaldoverdugo@MBP.local>
2016-10-18 21:15:34 -0400

    Favorite things completed

--- step4.1-favoriteThings-incomplete/FavoriteThings/ViewController.swift
+++ step4.1-favoriteThings-incomplete/FavoriteThings/ViewController.swift
@@ -15,31 +15,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // MARK: Model
     
     let favoriteThings: [String] = [
-        //TODO: Fill this array with your favorite things. Then use this collection to populate your table.
+        "Netflix and Chill with the couple",
+        "Sweet condensed milk",
+        "Pasta",
+        "Money"
     ]
 
     // MARK: Table View Data Source Methods
     
     // number of rows
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      
-        // TODO: Implement this method to get the correct row count
-        let placeholderCount = 2
-        return placeholderCount
+        return favoriteThings.count
     }
 
     
     // cell for row at index path
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
       
-      // TODO: Implement method
-      // 1. Dequeue a reusable cell from the table, using the correct “reuse identifier”
-      // 2. Find the model object that corresponds to that row
-      // 3. Set the images and labels in the cell with the data from the model object
-      // 4. return the cell.
-        
-        let placeholderCell = UITableViewCell()
-        return placeholderCell
+      let cell = tableView.dequeueReusableCellWithIdentifier("FavoriteThingCell")
+        cell?.textLabel?.text = favoriteThings[indexPath.row]
+        return cell!
     }
 }
 
